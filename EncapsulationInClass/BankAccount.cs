6aecee4f7c80af0fea095ca7e3cee47ed50437d1
mvdoyle <mdoyle@truecoders.io>
2020-06-30 12:08:59 -0500

    in class example

---
+++ EncapsulationInClass/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace EncapsulationInClass
+{
+    public class BankAccount
+    {
+        private double balance;
+
+        public BankAccount(double amount)
+        {
+            balance = amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;  
+        }
+
+        public void Withdraw(double amount)
+        {
+            balance -= amount;
+        }
+    }
+}
---
+++ EncapsulationInClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EncapsulationInClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mAcct = new BankAccount(400);
+
+            mAcct.Deposit(30000);
+
+            var bal = mAcct.GetBalance();
+
+            mAcct.Withdraw(300);
+        }
+    }
+}
